9a139268110f67e1fb4e7db6e45935d879eae3cf
telierlabs <zixksuper@gmail.com>
2025-12-31 20:22:15 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'xcode-v1';
+const assets = [
+  './',
+  './index.html',
+  './Vynix.webp'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
